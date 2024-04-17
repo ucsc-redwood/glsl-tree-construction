ac8e9cccdc768189aa040141504b1856abeac66f
Shaan Mistry <shaanmistry03@gmail.com>
2024-04-17 01:13:43 -0700

    save

--- radixsort-vulkan-cpp/benchmarks/benchmark.cpp
+++ radixsort-vulkan-cpp/benchmarks/benchmark.cpp
@@ -8,8 +8,9 @@
 
 namespace bm = benchmark;
 
-#define BUFFER_ELEMENTS 131072
+#define BUFFER_ELEMENTS 1920 * 1080
 #define MAX_BLOCKS 128
+#define ITERATIONS 20
 
 class PipeBenchmark : public Pipe
 {
@@ -20,9 +21,6 @@ public:
     void BM_GPU_Morton(bm::State &st)
     {
         int n_blocks = st.range(0);
-        // std::fill_n(u_points, params_.n, glm::vec4(0.0f, 0.0f, 0.0f, 0.0f));
-        // std::fill_n(u_morton_keys, params_.n, 0);
-
         for (auto _ : st)
         {
             morton(n_blocks, 0);
@@ -36,15 +34,19 @@ public:
         constexpr auto radix = 256;
         constexpr auto passes = 4;
         const auto binning_thread_blocks = (params_.n + 7680 - 1) / 7680;
-
+        int iters = 1;
         for (auto _ : st)
         {
             radix_sort(n_blocks, 0);
             st.SetIterationTime(time());
-            std::fill_n(sort_tmp.u_sort_alt, params_.n, 0);
-            std::fill_n(sort_tmp.u_global_histogram, radix * passes, 0);
-            std::fill_n(sort_tmp.u_pass_histogram, radix * binning_thread_blocks, glm::uvec4(0, 0, 0, 0));
-            std::fill_n(sort_tmp.u_index, 4, 0);
+            if (n_blocks != MAX_BLOCKS || iters < ITERATIONS)
+            {
+                std::fill_n(sort_tmp.u_sort_alt, params_.n, 0);
+                std::fill_n(sort_tmp.u_global_histogram, radix * passes, 0);
+                std::fill_n(sort_tmp.u_pass_histogram, radix * binning_thread_blocks, glm::uvec4(0, 0, 0, 0));
+                std::fill_n(sort_tmp.u_index, 4, 0);
+            }
+            iters++;
         }
     }
 
@@ -53,23 +55,58 @@ public:
         int n_blocks = st.range(0);
         uint32_t aligned_size = ((params_.n + 4 - 1) / 4) * 4;
         const uint32_t num_blocks = (aligned_size + PARTITION_SIZE - 1) / PARTITION_SIZE;
+        int iters = 1;
         for (auto _ : st)
         {
             unique(n_blocks, 0);
             st.SetIterationTime(time());
-            std::fill_n(u_unique_morton_keys, params_.n, 0);
-            std::fill_n(unique_tmp.contributions, params_.n, 0);
-            std::fill_n(unique_tmp.reductions, num_blocks, 0);
-            std::fill_n(unique_tmp.index, 1, 0);
+            if (n_blocks != MAX_BLOCKS || iters < ITERATIONS)
+            {
+                std::fill_n(u_unique_morton_keys, params_.n, 0);
+                std::fill_n(unique_tmp.contributions, params_.n, 0);
+                std::fill_n(unique_tmp.reductions, num_blocks, 0);
+                std::fill_n(unique_tmp.index, 1, 0);
+            }
+            iters++;
         }
     }
 
     void BM_GPU_RadixTree(bm::State &st)
     {
         int n_blocks = st.range(0);
+        // Cache data
+        uint32_t *u_copy_morton_keys = (uint32_t *)malloc(params_.n * sizeof(uint32_t));
+        memcpy(u_copy_morton_keys, u_unique_morton_keys, params_.n * sizeof(uint32_t));
+        int iters = 1;
         for (auto _ : st)
         {
             radix_tree(n_blocks, 0);
+            st.SetIterationTime(time());
+            // Reset data for next iteration
+            if (n_blocks != MAX_BLOCKS || iters < ITERATIONS)
+            {
+                memcpy(u_unique_morton_keys, u_copy_morton_keys, params_.n * sizeof(uint32_t));
+            }
+            iters++;
+        }
+    }
+
+    void BM_GPU_EdgeCount(bm::State &st)
+    {
+        int n_blocks = st.range(0);
+        for (auto _ : st)
+        {
+            edge_count(n_blocks, 0);
+            st.SetIterationTime(time());
+        }
+    }
+
+    void BM_GPU_Octree(bm::State &st)
+    {
+        int n_blocks = st.range(0);
+        for (auto _ : st)
+        {
+            octree(n_blocks, 0);
             st.SetIterationTime(time());
         }
     }
@@ -83,6 +120,7 @@ static void RegisterBenchmarks(PipeBenchmark &BenchmarkInstance)
         ->Unit(benchmark::kMillisecond)
         ->RangeMultiplier(2)
         ->Range(1, MAX_BLOCKS)
+        ->Iterations(ITERATIONS)
         ->ArgName("GridSize");
 
     benchmark::RegisterBenchmark("BM_GPU_Sort", [&BenchmarkInstance](benchmark::State &st)
@@ -91,6 +129,7 @@ static void RegisterBenchmarks(PipeBenchmark &BenchmarkInstance)
         ->Unit(benchmark::kMillisecond)
         ->RangeMultiplier(2)
         ->Range(1, MAX_BLOCKS)
+        ->Iterations(ITERATIONS)
         ->ArgName("GridSize");
 
     benchmark::RegisterBenchmark("BM_GPU_Unique", [&BenchmarkInstance](benchmark::State &st)
@@ -99,15 +138,32 @@ static void RegisterBenchmarks(PipeBenchmark &BenchmarkInstance)
         ->Unit(benchmark::kMillisecond)
         ->RangeMultiplier(2)
         ->Range(1, MAX_BLOCKS)
+        ->Iterations(ITERATIONS)
         ->ArgName("GridSize");
 
-    // Added BM_GPU_RadixTree benchmark
     benchmark::RegisterBenchmark("BM_GPU_RadixTree", [&BenchmarkInstance](benchmark::State &st)
                                  { BenchmarkInstance.BM_GPU_RadixTree(st); })
         ->UseManualTime()
         ->Unit(benchmark::kMillisecond)
         ->RangeMultiplier(2)
         ->Range(1, MAX_BLOCKS)
+        ->Iterations(ITERATIONS)
+        ->ArgName("GridSize");
+
+    benchmark::RegisterBenchmark("BM_GPU_EdgeCount", [&BenchmarkInstance](benchmark::State &st)
+                                 { BenchmarkInstance.BM_GPU_EdgeCount(st); })
+        ->Unit(benchmark::kMillisecond)
+        ->RangeMultiplier(2)
+        ->Range(1, MAX_BLOCKS)
+        ->Iterations(ITERATIONS)
+        ->ArgName("GridSize");
+
+    benchmark::RegisterBenchmark("BM_GPU_Octree", [&BenchmarkInstance](benchmark::State &st)
+                                 { BenchmarkInstance.BM_GPU_Octree(st); })
+        ->Unit(benchmark::kMillisecond)
+        ->RangeMultiplier(2)
+        ->Range(1, MAX_BLOCKS)
+        ->Iterations(ITERATIONS)
         ->ArgName("GridSize");
 }
 
